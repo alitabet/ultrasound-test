b90adaaaddc0c542177b575b6c8f2b69783e3b90
Ali Thabet <alitabet@gmail.com>
2017-06-01 13:39:24 +0300

    Fix config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'http://ec2-13-58-174-10.us-east-2.compute.amazonaws.com',
+      host: 'ec2-13-58-174-10.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/ultrasound-key.pem',
       ref: 'origin/master',
       repo: 'git@github.com:alitabet/ultrasound-test.git',
