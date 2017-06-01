d8ed40288bb267192cd71234711473a1db2fc924
Ali Thabet <alitabet@gmail.com>
2017-06-01 13:23:03 +0300

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'ultrasound-aws',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'http://ec2-13-58-174-10.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/ultrasound-key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:alitabet/ultrasound-test.git',
+      path: '/home/ubuntu/ultrasound-aws',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
